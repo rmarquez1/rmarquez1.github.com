7b91caf79556dea517cc5595eb43eeffcca4f4a0
rmarquez1 <albertocomp10@gmail.com>
2021-06-03 20:01:11 -0400

    Nuevo proyecto boletines
    Este es un proyecto hecho con el Curso Profesional de Frontend.

--- boletines/javascript/main.js
+++ boletines/javascript/main.js
@@ -62,7 +62,7 @@ links.forEach(function(link) {
 		content.classList.add("animate__fadeOutUp");
 
 		setTimeout(function() {
-			location.href = "/";
+			location.href = "/boletines";
 		}, 600);
 
 	});
